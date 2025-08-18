ed145a461707319e0f76c18dc88e0aafb314780f
alelodato <alss.ldt@gmail.com>
2025-08-18 12:00:10 +0200

    added sitemap page

--- assets/js/script.js
+++ assets/js/script.js
@@ -57,10 +57,19 @@ document.getElementById('contact-form').addEventListener('submit', function (e) 
 
   emailjs.sendForm('service_sv9m58a', 'template_33hwqck', '#contact-form')
     .then(() => {
-      Swal.fire({
-      title: "Message sent successfully!",
-      text: "Thank you for your message, i will reply as soon as possible!",
-    });
+      const lang = localStorage.getItem('language');
+      if (lang === 'en') {
+        Swal.fire({
+        title: "Message sent successfully!",
+        text: "Thank you for your message, i will reply as soon as possible!",
+      });
+      } else {
+        Swal.fire({
+        title: "Messaggio inviato!",
+        text: "Grazie per il tuo messaggio, risponderó il prima possibile!",
+      });
+      }
+      
       this.reset();
     }, (error) => {
       alert('Failed to send message. Please try again.');
