eb4abc1d6cd037977ab533eafb270e7ceb70a0d7
alelodato <alss.ldt@gmail.com>
2025-09-29 16:56:36 +0700

    adjusted image carousel to work on both sections

--- assets/js/carousel.js
+++ assets/js/carousel.js
@@ -1,34 +1,32 @@
-const track = document.querySelector('.carousel-track');
-const images = document.querySelectorAll('.carousel-track img');
-const prevButton = document.querySelector('.prev');
-const nextButton = document.querySelector('.next');
+document.querySelectorAll('.carousel').forEach(carousel => {
+  const track = carousel.querySelector('.carousel-track');
+  const images = carousel.querySelectorAll('img');
+  const prevButton = carousel.querySelector('.prev');
+  const nextButton = carousel.querySelector('.next');
 
-let index = 0;
+  let index = 0;
 
-function updateCarousel() {
-  const width = track.parentElement.clientWidth;
-  track.style.transform = `translateX(${-index * width}px)`;
-
-  // Disabilita/abilita i bottoni ai bordi
-  prevButton.disabled = index === 0;
-  nextButton.disabled = index === images.length - 1;
-}
-
-nextButton.addEventListener('click', () => {
-  if (index < images.length - 1) {
-    index++;
-    updateCarousel();
+  function updateCarousel() {
+    const width = track.parentElement.clientWidth;
+    track.style.transform = `translateX(${-index * width}px)`;
+    prevButton.disabled = index === 0;
+    nextButton.disabled = index === images.length - 1;
   }
+
+  nextButton.addEventListener('click', () => {
+    if (index < images.length - 1) {
+      index++;
+      updateCarousel();
+    }
+  });
+
+  prevButton.addEventListener('click', () => {
+    if (index > 0) {
+      index--;
+      updateCarousel();
+    }
+  });
+
+  window.addEventListener('resize', updateCarousel);
+  updateCarousel();
 });
-
-prevButton.addEventListener('click', () => {
-  if (index > 0) {
-    index--;
-    updateCarousel();
-  }
-});
-
-window.addEventListener('resize', updateCarousel);
-
-// Avvio iniziale
-updateCarousel();
